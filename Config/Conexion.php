b07dcef8bd9ae3be5a93f27795bc9fcd1697d040
Camilo <juancamilog9911@gmail.com>
2024-11-11 16:56:33 -0500

    cambios de credenciales

--- Config/Conexion.php
+++ Config/Conexion.php
@@ -4,10 +4,10 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 
-$host = 'bfromhyapic8gh9mkwcu-mysql.services.clever-cloud.com';
-$databasename ='bfromhyapic8gh9mkwcu';
-$user = 'uoovfcgryqtxbrhy';
-$password = 'rYzoOJeJIGSI1SjIBbFw';
+$host = 'bq0vfnib8muottxijajb-mysql.services.clever-cloud.com';
+$databasename ='bq0vfnib8muottxijajb';
+$user = 'uxirala7zhifttwm';
+$password = 'BUrCTq9L7CA48YCRM8OA';
 
 try{
     $base_de_datos = new PDO("mysql:host=$host; dbname=$databasename", $user, $password);
